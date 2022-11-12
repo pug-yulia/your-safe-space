abc36186ab744013bac4693996f45f40076eb712
Yulia <ybe2004ybe@gmail.com>
2022-11-12 19:41:25 +0200

    update

---
+++ navbar.js
@@ -0,0 +1,6 @@
+const toggleButton = document.getElementsByClassName("toggle-button")[0]
+const links = document.getElementsByClassName("links")[0]
+
+toggleButton.addEventListener("click", () => {
+    links.classList.toggle("active")
+})
