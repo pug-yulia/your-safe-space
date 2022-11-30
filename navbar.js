6e2410811611616d3820eeac1ac90f6c466b7b9d
Yulia <ybe2004ybe@gmail.com>
2022-11-30 22:01:04 +0200

    major update

--- navbar.js
+++ navbar.js
@@ -1,5 +1,5 @@
 const toggleButton = document.getElementsByClassName("toggle-button")[0]
-const links = document.getElementsByClassName("links")[0]
+const links = document.getElementsByClassName("navbar-links")[0]
 
 toggleButton.addEventListener("click", () => {
     links.classList.toggle("active")
